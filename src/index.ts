c69e5809c75ad4414da300b0a3e02c4cfd0508dd
yashbalyan08-IQOL <yash.balyan@truestate.in>
2025-07-10 14:38:39 +0530

    port changed

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import notificationRoutes from "./routes/notificationRoutes";
-import { startDelistingCron } from "../src/cron/delistingCron"
+import { startDelistingCron } from "../src/cron/delistingCron";
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.use(cors({
-  origin: true,
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 app.listen(port, () => {
